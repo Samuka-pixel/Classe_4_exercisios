d5a7606def3703924c8999958eda9b8e3f6a81eb
Samuka-pixel <shark06778@sharkcoders.pt>
2024-10-26 14:17:39 +0100

    Update conv_dist.py

--- conv_dist.py
+++ conv_dist.py
@@ -3,5 +3,21 @@ med = str(input("qual é a medida que queres?: "))
 if med == "km":
     medEmKm = m/1000
     print(medEmKm, "km")
-else:
-    print(m * 3.281, "pés")
+if med == "hm":
+    medEmKm = m/100
+    print(medEmKm, "hm")
+if med == "dam":
+    medEmKm = m / 10
+    print(medEmKm, "dam")
+if med == "m":
+    medEmKm = m
+    print(medEmKm, "m")
+if med == "dm":
+    medEmKm = m * 10
+    print(medEmKm, "dm")
+if med == "cm":
+    medEmKm = m * 100
+    print(medEmKm, "cm")
+if med == "mm":
+    medEmKm = m * 1000
+    print(medEmKm, "mm")
